8b84be0fe1338e2830982b49ad4f1cd21e024098
Nelson <nyen@salesforce.com>
2020-07-03 16:02:43 -0700

    init_prefix_table fix in python script.
    Thanks to @Range0122 for submitting fix!

--- KMP.py
+++ KMP.py
@@ -1,21 +1,22 @@
 #
-# Implementation of Knuth-Morris-Pratt (KMP) Algorithm 
+# Implementation of Knuth-Morris-Pratt (KMP) Algorithm
 # anontated.
 #
-# Algorithm creates a text table for use with searches. 
+# Algorithm creates a text table for use with searches.
 #
 # Running Time is O(n+m), where 'n' is length of the searched text (typically large)
 # and 'm' is the length of pattern (or pattern, typically small).
 #
-# Assumptions: length of pattern <= length of text, e.g. m <= n, otherwise 
+# Assumptions: length of pattern <= length of text, e.g. m <= n, otherwise
 # function will not find the pattern
 #
+import itertools
 
 def kmp_match(text, pattern):
     # By checking the largest word suffix which matches the word prefix, we can
-    # generate a 'prefix table' that can then be used as a state machine, where 0 is the 
+    # generate a 'prefix table' that can then be used as a state machine, where 0 is the
     # initial state and the last character in the table being the accpeting state.
-    # 
+    #
     # We generate the prefix table by maintaining a running count of character matches
     # starting from the 2nd character as we are walking the word character by character.
     #
@@ -26,26 +27,27 @@ def kmp_match(text, pattern):
     #
 
     def init_prefix_table(word):
-        prefix_count = 0
-        prefix_table = [0]*len(word)
-        # start from the 2nd character
-        for i in range(1, len(word)):
-            if word[i] == word[prefix_count]:
-                prefix_count+=1
+        prefix_table = [0] * (len(word) + 1)
+        prefix_table[0] = -1
+        i, j = 0, -1
+        while i < len(word):
+            if j == -1 or word[i] == word[j]:
+                i += 1
+                j += 1
+                prefix_table[i] = j
             else:
-                prefix_count = 0
-            prefix_table[i] = prefix_count
-        return prefix_table
+                j = prefix_table[j]
+        return prefix_table[1:]
 
     prefix_table = init_prefix_table(pattern)
-    #print prefix_table
+    #print ('prefix table:', prefix_table)
 
     # now that we have the state_table (aka prefix table) created, we can do the string matching
     # using it as a guide for state transitions
     pattern_index = 0
     for text_index, text_ch in enumerate(text):
-        # Check for failure; if failure, then transition to the correct 
-        # intermediate state. If no intermediate state is found, then we will go 
+        # Check for failure; if failure, then transition to the correct
+        # intermediate state. If no intermediate state is found, then we will go
         # to the initial state and thus exit the while loop
         # the state variable also informs the correct index into the pattern string
         while pattern_index > 0 and text_ch != pattern[pattern_index]:
@@ -61,68 +63,89 @@ def kmp_match(text, pattern):
 
 
 def main():
-    print 'Knuth Morris Pratt (KMP) string search algorithm'
+    print('Knuth Morris Pratt (KMP) string search algorithm')
 
     Input = 'y ababaca siifjae', 'ababaca'
-    print Input
+    print(Input)
     res = kmp_match(Input[0], Input[1])
-    print 'len:', len(Input[1])
-    print 'result:', res, "'{}'".format(Input[0][res:res+len(Input[1])])
+    print('len:', len(Input[1]))
+    print('result:', res, "'{}'".format(Input[0][res:res+len(Input[1])]))
     assert Input[0][res:res+len(Input[1])] == Input[1]
-    print 'correct:', Input[0].find(Input[1])
+    print('correct:', Input[0].find(Input[1]))
     assert res == Input[0].find(Input[1])
 
     Input = 'y ababac siifjae', 'ababac'
-    print Input
+    print(Input)
     res = kmp_match(Input[0], Input[1])
-    print 'len:', len(Input[1])
-    print res, "'{}'".format(Input[0][res:res+len(Input[1])])
+    print('len:', len(Input[1]))
+    print(res, "'{}'".format(Input[0][res:res+len(Input[1])]))
     assert Input[0][res:res+len(Input[1])] == Input[1]
-    print 'correct:', Input[0].find(Input[1])
+    print('correct:', Input[0].find(Input[1]))
     assert res == Input[0].find(Input[1])
 
     Input = 'abababacsiifjae', 'ababac'
-    print Input
+    print (Input)
     res = kmp_match(Input[0], Input[1])
-    print 'len:', len(Input[1])
-    print res, "'{}'".format(Input[0][res:res+len(Input[1])])
-    print 'correct:', Input[0].find(Input[1])
+    print ('len:', len(Input[1]))
+    print (res, "'{}'".format(Input[0][res:res+len(Input[1])]))
+    print ('correct:', Input[0].find(Input[1]))
     assert Input[0][res:res+len(Input[1])] == Input[1]
     assert res == Input[0].find(Input[1])
 
     Input = 'adfbec dsfoeifj asdfjww abyyabcdefg siifjae', 'aba'
+    print (Input)
     res = kmp_match(Input[0], Input[1])
-    print 'len:', len(Input[1])
-    print res 
-    print 'correct:', Input[0].find(Input[1])
+    print ('len:', len(Input[1]))
+    print (res)
+    print ('correct:', Input[0].find(Input[1]))
     assert res == Input[0].find(Input[1])
 
     Input = 'adfbec dsfoeifj asdfjww sifmyy abcdefg siifjae', 'ab'
+    print (Input)
     res = kmp_match(Input[0], Input[1])
-    print 'len:', len(Input[1])
-    print res, "'{}'".format(Input[0][res:res+len(Input[1])])
-    print 'correct:', Input[0].find(Input[1])
+    print ('len:', len(Input[1]))
+    print (res, "'{}'".format(Input[0][res:res+len(Input[1])]))
+    print ('correct:', Input[0].find(Input[1]))
     assert res == Input[0].find(Input[1])
 
     Input = 'adfbec dsfoeifj asdfjww sifmyy abcdefg siifjae', 'a'
+    print (Input)
     res = kmp_match(Input[0], Input[1])
-    print 'len:', len(Input[1])
-    print res, "'{}'".format(Input[0][res:res+len(Input[1])])
-    print 'correct:', Input[0].find(Input[1])
+    print ('len:', len(Input[1]))
+    print (res, "'{}'".format(Input[0][res:res+len(Input[1])]))
+    print ('correct:', Input[0].find(Input[1]))
     assert res == Input[0].find(Input[1])
 
     Input = 'adfbec dsfoeifj asdfjww sifmyy abcdefg siifjae', 'aa'
+    print (Input)
     res = kmp_match(Input[0], Input[1])
-    print 'len:', len(Input[1])
-    print res, "'{}'".format(Input[0][res:res+len(Input[1])])
-    print 'correct:', Input[0].find(Input[1])
+    print ('len:', len(Input[1]))
+    print (res, "'{}'".format(Input[0][res:res+len(Input[1])]))
+    print ('correct:', Input[0].find(Input[1]))
     assert res == Input[0].find(Input[1])
-    
+
     Input = "ababac", "ababac",
+    print (Input)
     res = kmp_match(Input[0], Input[1])
-    print 'len:', len(Input[1])
-    print res, "'{}'".format(Input[0][res:res+len(Input[1])])
-    print 'correct:', Input[0].find(Input[1])
+    print ('len:', len(Input[1]))
+    print (res, "'{}'".format(Input[0][res:res+len(Input[1])]))
+    print ('correct:', Input[0].find(Input[1]))
+    assert res == Input[0].find(Input[1])
+
+    Input =  "aabaaabaaac", "aabaaac"
+    print (Input)
+    res = kmp_match(Input[0], Input[1])
+    print ('len:', len(Input[1]))
+    print (res, "'{}'".format(Input[0][res:res+len(Input[1])]))
+    print ('correct:', Input[0].find(Input[1]))
+    assert res == Input[0].find(Input[1])
+
+    Input = 'bacbababaabcbab', 'ababababca'
+    print (Input)
+    res = kmp_match(Input[0], Input[1])
+    print ('len:', len(Input[1]))
+    print ('res:', res, "'{}'".format(Input[0][res:res+len(Input[1])]))
+    print ('correct:', Input[0].find(Input[1]))
     assert res == Input[0].find(Input[1])
  
 
